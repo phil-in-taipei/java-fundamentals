6aaaf5e6a167b43d90b00d9fa0ad4626c261cec0
mikalikahi <sweeney.phil@gmail.com>
2022-09-07 10:39:45 +0800

    Some basic testing of the IDE and it's functionality in exercise 3 and an example file

---
+++ src/labs_examples/fundamentals/labs/Example.java
@@ -0,0 +1,15 @@
+package labs_examples.fundamentals.labs;
+
+public class Example {
+    public static void main(String[] args) {
+
+        int result = multiply(12, 675);
+        System.out.println("Its a great day");
+        System.out.println("The result of multiplying 12 and 675 is:");
+        System.out.println(result);
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
--- src/labs_examples/fundamentals/labs/Exercise_03.java
+++ src/labs_examples/fundamentals/labs/Exercise_03.java
@@ -14,11 +14,20 @@ public class Exercise_03 {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        //System.out.println("Hello World!");
 
         // write code to print out "Check out my first program!" below
-	
-	System.out.println("Phil Test");
+        System.out.println("Hello World!");
+        System.out.println("Check out my new program!");
+
+        // debugger example:
+        int i = 10;
+        int x = 12;
+        int z = i + x;
+        System.out.println("The total of 10 + 12 is:");
+        System.out.println(z);
+
+	//System.out.println("Phil Test");
     }
 
 }
