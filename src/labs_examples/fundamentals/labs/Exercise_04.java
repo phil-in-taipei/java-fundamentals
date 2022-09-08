b571608150f34136d03d8950c093df29f9777055
mikalikahi <sweeney.phil@gmail.com>
2022-09-08 14:53:50 +0800

    Did the exercises in fundamentals sections, and learned that I have to remember to add 'ln' to the end of a print statement :)

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -12,10 +12,47 @@ public class Exercise_04 {
 
     public static void main(String[] args) {
 
-        int i = 1;
-        System.out.println("int i is: " + i);
+        System.out.println("These are java primitive examples:");
+        System.out.println("----------------------------------");
+        System.out.println("");
 
-        // write your code below
+        boolean isTrue = true;
+        System.out.println("1st: boolean: " + isTrue);
+
+        System.out.println("");
+
+        char charVal = '囧';
+        System.out.println("2nd: char: " + charVal);
+
+        System.out.println("");
+
+        byte byteVal = 10;
+        System.out.println("3rd: " + byteVal);
+
+        System.out.println("");
+
+        short shortVal = 10000;
+        System.out.println("4th: short: " + shortVal);
+
+        System.out.println("");
+
+        int intVal = 93889;
+        System.out.println("5th: int: " + intVal);
+
+        System.out.println("");
+
+        long longVal = 2847982828282023889l;
+        System.out.println("6th: long: " + longVal);
+
+        System.out.println("");
+
+        float floatVal = 1331321.0023479392972f;
+        System.out.println("7th: float: " + floatVal);
+
+        System.out.println("");
+
+        double doubleVal = 82029031321.0023d;
+        System.out.println("8th: double: " + doubleVal);
 
     }
 
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,16 +14,79 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        System.out.print("");
+        int lenOfHello = str.length();
+
+        System.out.println(
+                "The length of the word hello is: ".concat(
+                    Integer.toString(lenOfHello)
+                ));
 
         String str2 = "hello";
+
         // please initialize a boolean variable and test whether str is equal to str2
-
+        // they aren't equal, but their values are the same
+        System.out.println("");
+        boolean stringsAreEqual = str.equals(str2);
+        System.out.println("The two strings are the equal: " + stringsAreEqual);
+        System.out.println("");
+        System.out.println(
+                "The two strings are the same: " + confirmStringsAreSame(str, str2)
+        );
+        System.out.println("");
         // please concatenate str & str2 and set the result to a new String variable below
-
+        String str3 = str.concat(str2);
+        System.out.println(str3);
+        String concatShouldBe = "hello!hello";
+        System.out.println("");
+        System.out.println(
+                "This string is a concatenation of the first two strings: " + str3
+        );
+        System.out.println("");
+        System.out.println(
+                "The concatenated string was what I expected: " + confirmStringsAreSame(str3, concatShouldBe)
+        );
+        System.out.println("");
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+        boolean stringContainsI = "string".contains("i");
+        System.out.println(
+                "There's not no 'i' in string: " + stringContainsI
+        );
 
+        System.out.println("");
+        String hopeMySkills = "string".replace("i", "o");
+        System.out.println(
+                "I hope my skills will become: " + hopeMySkills
+        );
+        System.out.println("");
+        String codingStandard = "Stringently".substring(0, 9);
+        System.out.println("I should apply " + codingStandard + " standards");
+        System.out.println("");
+        int indexOfO = str.indexOf('o');
+        System.out.println(
+                "The character o is at this index in the word 'hello': " + indexOfO
+        );
     }
 
+    public static boolean confirmStringsAreSame(String str1, String str2) {
 
+        int l1 = str1.length();
+        int l2 = str2.length();
+        boolean stringLengthsAreSame = l1 == l2;
+        boolean stringAreSameTheory = false;
+        if (stringLengthsAreSame) {
+            stringAreSameTheory = true;
+            for (int i = 0; i < l2; i++) {
+                char str1_ch = str1.charAt(i);
+                char str2_ch = str2.charAt(i);
+                if (str1_ch != str2_ch) {
+                    stringAreSameTheory = false;
+                } else {
+                    stringAreSameTheory = true;
+                }
+            }
+        }
+        return stringAreSameTheory;
+    }
 }
