d3f27465325fb391cf56cbfcfb6dbb9940f004a8
mikalikahi <sweeney.phil@gmail.com>
2022-09-08 15:03:42 +0800

    Put more spaces in the exercise and comments to make the code more readable

--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -34,6 +34,8 @@ public class Exercise_05 {
                 "The two strings are the same: " + confirmStringsAreSame(str, str2)
         );
         System.out.println("");
+
+
         // please concatenate str & str2 and set the result to a new String variable below
         String str3 = str.concat(str2);
         System.out.println(str3);
@@ -42,27 +44,41 @@ public class Exercise_05 {
         System.out.println(
                 "This string is a concatenation of the first two strings: " + str3
         );
+
+
         System.out.println("");
+
+        // for fun, I called another method, that checks if the strings are the same
         System.out.println(
                 "The concatenated string was what I expected: " + confirmStringsAreSame(str3, concatShouldBe)
         );
         System.out.println("");
+
+
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+
+        // contains example
         boolean stringContainsI = "string".contains("i");
         System.out.println(
                 "There's not no 'i' in string: " + stringContainsI
         );
 
         System.out.println("");
+
+        // replace example
         String hopeMySkills = "string".replace("i", "o");
         System.out.println(
                 "I hope my skills will become: " + hopeMySkills
         );
         System.out.println("");
+
+        // substring example
         String codingStandard = "Stringently".substring(0, 9);
         System.out.println("I should apply " + codingStandard + " standards");
         System.out.println("");
+
+        // index example
         int indexOfO = str.indexOf('o');
         System.out.println(
                 "The character o is at this index in the word 'hello': " + indexOfO
@@ -70,11 +86,17 @@ public class Exercise_05 {
     }
 
     public static boolean confirmStringsAreSame(String str1, String str2) {
+        // this returns a boolean confirming whether
+        // the strings have
+        // the same values
 
         int l1 = str1.length();
         int l2 = str2.length();
+
         boolean stringLengthsAreSame = l1 == l2;
+
         boolean stringAreSameTheory = false;
+
         if (stringLengthsAreSame) {
             stringAreSameTheory = true;
             for (int i = 0; i < l2; i++) {
@@ -87,6 +109,7 @@ public class Exercise_05 {
                 }
             }
         }
+        
         return stringAreSameTheory;
     }
 }
