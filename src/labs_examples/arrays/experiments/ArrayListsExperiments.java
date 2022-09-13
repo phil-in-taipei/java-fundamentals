a63b920922f28e48f8b9a7bfb81066309a80dfcc
mikalikahi <sweeney.phil@gmail.com>
2022-09-13 21:51:30 +0800

    Added a file to experiment with Array Lists

--- src/labs_examples/arrays/experiments/ArrayListsExperiments.java
+++ src/labs_examples/arrays/experiments/ArrayListsExperiments.java
@@ -36,5 +36,73 @@ public class ArrayListsExperiments {
         }
 
         System.out.println("---------------------------------------");
+        ArrayList<String> list = new ArrayList<String>(10) ;
+        list.add( "Ana" );
+
+        list.add( "Bob" );
+        list.add( "Cathy" );
+        list.add( 0, "Eric" );
+        System.out.println(list.get(2));
+
+        // please declare an array or arraylist below
+        System.out.println("");
+        System.out.println("--------------------------------------");
+        System.out.println("This is an array list of the Alphabet:");
+        System.out.println("--------------------------------------");
+        ArrayList<String> alphabet = new ArrayList<String>();
+
+        // please populate that array or arraylist
+        for (char c = 'a'; c <= 'z'; c++) {
+            String s = "" + c;
+            alphabet.add(s);
+        }
+        // please use a For Each Loop to print each element
+        // in the array or arraylist you declared and populated above
+        int index = 1;
+        for (String s : alphabet) {
+            System.out.println("Item " + index + " in the list is: " + s);
+            index++;
+        }
+
+        System.out.println("--------------------------------------");
+        System.out.println("OK, done!");
+        System.out.println("--------------------------------------");
+
+        //YOUR CODE BELOW
+
+        /*  1.  First create and initialize an ArrayList called guests.
+                Remember, the syntax to create an ArrayList is:
+                ArrayList<type> name = new ArrayList<type>();   */
+        ArrayList<String> guests = new ArrayList<String>();
+
+        /*  2.  Now add the following names to the list using .add() :
+                Arnold, Gerald, Eugene, Helga, Phoebe in that order */
+        guests.add("Arnold");
+        guests.add("Gerald");
+        guests.add("Eugene");
+        guests.add("Helga");
+        guests.add("Phoebe");
+
+
+        /*  3.  Print the names using a ForEach loop.
+                Remember, the syntax for the ForEach loop is:
+                for(type element: collection) { code }          */
+        for (String guest: guests) {
+            System.out.println(guest);
+        }
+
+        /*  4.  Eugene wont be able to make it.
+                Remove him from the list using .remove() */
+        guests.remove("Eugene");
+
+        /*  5. Print the list again  */
+        System.out.print("| ");
+        for (String guest: guests) {
+            System.out.print(guest + " | ");
+        }
+
+
+        //YOUR CODE ABOVE
+
     }
 }
