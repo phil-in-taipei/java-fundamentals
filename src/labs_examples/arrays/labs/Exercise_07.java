995c58ba0805f8814d15e98d6c552fe58b04629a
mikalikahi <sweeney.phil@gmail.com>
2022-09-14 12:12:02 +0800

    Added last arrays exercise

--- src/labs_examples/arrays/labs/Exercise_07.java
+++ src/labs_examples/arrays/labs/Exercise_07.java
@@ -1,5 +1,7 @@
 package labs_examples.arrays.labs;
 
+import java.util.ArrayList;
+
 /**
  *  ArrayLists
  *
@@ -10,4 +12,55 @@ package labs_examples.arrays.labs;
  *
  */
 public class Exercise_07 {
+    public static void main(String[] args) {
+        ArrayList<String> arrList = new ArrayList();
+        System.out.println("");
+        System.out.println("---------------------------------------");
+        System.out.println("This is an array list:");
+        System.out.println("---------------------------------------");
+
+        arrList.add("Let's");
+        arrList.add("all");
+        arrList.add("get");
+        arrList.add("going");
+        arrList.add("and");
+        arrList.add("have");
+        arrList.add("some");
+        arrList.add("fun");
+        arrList.add("!");
+
+
+        arrList.remove(1);
+
+        for (String s : arrList) {
+            System.out.print(s + " ");
+        }
+        System.out.println("");
+        System.out.println("---------------------------------------");
+        System.out.println("This is some info about the array list:");
+        System.out.println("---------------------------------------");
+
+
+        if (!arrList.isEmpty()) {
+            System.out.println("This array list has this many items:" + arrList.size());
+            System.out.println("---------------------------------------");
+            if (arrList.indexOf("!") != -1) {
+                System.out.println("This array list has an exclamation mark");
+                System.out.println("---------------------------------------");
+            } else {
+                System.out.println("The array list is does not have an exclamation mark!");
+                System.out.println("---------------------------------------");
+            }
+            if (arrList.contains("fun")) {
+                System.out.println("This array list has fun!");
+                System.out.println("---------------------------------------");
+            } else {
+                System.out.println("The array list is does not have fun");
+                System.out.println("---------------------------------------");
+            }
+        } else {
+            System.out.println("The array list is empty!");
+        }
+
+    }
 }
