261dd35db504b9d7eeeee82fd0c4791acda1ab1c
mikalikahi <sweeney.phil@gmail.com>
2022-09-14 17:34:00 +0800

    Fixed a glitch in a 5X5 matrix of one of the array exercises

--- src/labs_examples/arrays/labs/Exercise_03.java
+++ src/labs_examples/arrays/labs/Exercise_03.java
@@ -18,20 +18,20 @@ package labs_examples.arrays.labs;
 public class Exercise_03 {
 
     public static void main(String[] args) {
-        int[][] TwoDArray = new int[6][6];
+        int[][] TwoDArray = new int[5][5];
+        System.out.println(TwoDArray.length);
 
         System.out.println("------------------------------------------------------");
         System.out.println("This is a matrix with multiples of 3, starting with 3:");
         System.out.println("------------------------------------------------------");
         int multOfThree = 1;
-        for (int i = 0; i < TwoDArray.length; i++) {
-            multOfThree++;
-            for (int j = 0; j < TwoDArray[i].length; j++) {
-                if (i > 0 && j > 0) {
-                    TwoDArray[i][j] = i * j * multOfThree;
-                    System.out.print(TwoDArray[i][j] + " |");
-                }
+        for (int i = 1; i < 6; i++) {
+            for (int j = 1; j < 6; j++) {
+                TwoDArray[i -1][j -1] = multOfThree * 3;
+                System.out.print(TwoDArray[i -1][j -1] + " |");
+                multOfThree++;
             }
+
             System.out.println("");
         }
         System.out.println("----------------------------------------------------");
