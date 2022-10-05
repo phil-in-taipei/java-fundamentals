96896e3de4cecdd3b73fd581d44a46f6f1191e4d
mikalikahi <sweeney.phil@gmail.com>
2022-10-06 06:18:10 +0800

    Added file to be read

--- src/labs_examples/input_output/labs/Exercise_01.java
+++ src/labs_examples/input_output/labs/Exercise_01.java
@@ -1,4 +1,9 @@
 package labs_examples.input_output.labs;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.BufferedInputStream;
+
 
 /**
  * Input/Output Exercise 1: File input/output
@@ -10,10 +15,34 @@ package labs_examples.input_output.labs;
  */
 
 class Example {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // char_data.txt
+        FileInputStream inputStream = null;
+        BufferedInputStream bufferedInputStream = null;
 
+        try {
+            inputStream = new FileInputStream("data.txt");
+            bufferedInputStream = new BufferedInputStream(inputStream);
 
+            byte[] buffer = new byte[5];
+            int bytesRead = 0;
 
+            while ((bytesRead = bufferedInputStream.read(buffer)) != -1) {
+                // print out bytes read to console
+                System.out.print(new String(buffer, 0, bytesRead));
+            }
+        } catch (IOException exc) {
+            System.out.println("An error occurred: " + exc.getMessage());
+            exc.printStackTrace();
+        } finally {
+            // we be using the Try-With-Resources shortly to avoid the need for this
+            try {
+                inputStream.close();
+                bufferedInputStream.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
+
 }
