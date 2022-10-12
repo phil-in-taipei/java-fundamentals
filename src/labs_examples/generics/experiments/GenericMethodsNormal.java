d9f5a39328997a8d3f54c2460f7fab1a57c29d4d
mikalikahi <sweeney.phil@gmail.com>
2022-10-12 12:42:42 +0800

    Added some methods to experiments in generics section to practice generic types in methods

--- src/labs_examples/generics/experiments/GenericMethodsNormal.java
+++ src/labs_examples/generics/experiments/GenericMethodsNormal.java
@@ -1,5 +1,7 @@
 package labs_examples.generics.experiments;
 
+import java.util.ArrayList;
+
 public class GenericMethodsNormal {
     public static void main(String[] args) {
         // Step 1) create an array of 5 stings then call the printArray() method and
@@ -16,6 +18,12 @@ public class GenericMethodsNormal {
         Double[] doubleArrayVariable = { 11.5, 110.5, 10.9, 10.12, 10.5, };
         printArray(doubleArrayVariable);
 
+
+        // now use multiply with generic numbers:
+        System.out.println(multiply(10, 20));
+
+
+
     }
 
     public static <E> void printArray(E[] inputArray) {
@@ -24,5 +32,28 @@ public class GenericMethodsNormal {
             System.out.printf("%s ", element);
         }
         System.out.println();
+    }
+
+    public static <T extends Number, V extends Number> int multiply(T firstNum, V secondNum) {
+        return firstNum.intValue() * secondNum.intValue();
+    }
+
+    public static double sumCollection(ArrayList<? extends Number> nums) {
+        double sum = 0;
+        for (int i=0; i< nums.size(); i++) {
+            sum += (double) nums.get(i);
+        }
+        return sum;
+        // returns sum of array with any kind of numbers input
+    }
+
+    public static <S extends Number> void square(S val) {
+        System.out.println(val.intValue() * val.intValue());
+        // prints the int square of the number
+    }
+
+    public static <S> void doSomething(S val) {
+        System.out.println(val);
+        // prints an input regardless of type
     }
 }
