6c7a4646044e863ba67b482b95b7b65de4e4693f
mikalikahi <sweeney.phil@gmail.com>
2022-10-18 16:13:16 +0800

    Another prebuilt interface practice for lambdas lab

---
+++ src/labs_examples/lambdas/labs/BiPredicateExample.java
@@ -0,0 +1,52 @@
+package labs_examples.lambdas.labs;
+
+import java.util.function.BiPredicate;
+
+public class BiPredicateExample {
+    public static void main(String[] args) {
+        PrintInfoClass.printExerciseIntro("Bi-predicate Built-in");
+        PrintInfoClass.printDividerLine();
+        System.out.println("Takes in two integers and makes sure both are even");
+
+        BiPredicate<Integer, Integer> bothIntsEven = (n1, n2) -> (n1 % 2 == 0 && n2 % 2 ==0);
+        System.out.println(
+                "Test 120 and 6: "
+                + bothIntsEven.test(120, 6)
+        );
+        System.out.println(
+                "Test 130 and 5: "
+                +bothIntsEven.test(130, 5)
+        );
+
+        PrintInfoClass.printDividerLine();
+        System.out.println(
+                "Takes in String and integer and makes sure String is longer than 2 and int is greater than 4"
+        );
+        BiPredicate<String, Integer> nameAndAgeMinValid = (name, age) -> (name.length() > 2 && age > 4);
+        System.out.println(
+                "Test Joe and 20: "
+                + nameAndAgeMinValid.test("Joe", 20));
+        System.out.println(
+                "Test T and 100: "
+                + nameAndAgeMinValid.test("T", 100));
+
+
+        PrintInfoClass.printDividerLine();
+        System.out.println(
+                "Takes in Strings and makes sure they are not the same"
+        );
+
+        BiPredicate<String, String> givenSurnameDifferent = (givenName, surName) -> (!givenName.equals(surName));
+
+        System.out.println(
+                "Test Jack and Jack: "
+                + givenSurnameDifferent.test("Jack", "Jack")
+        );
+        System.out.println(
+                "Test negate Jack and Jack: "
+                + givenSurnameDifferent.negate().test("Jack", "Jack")
+        );
+
+        PrintInfoClass.printExerciseExit("Bi-predicate Built-in");
+    }
+}
