851a47b50ff564399d33e3ea1c658cd5eb7fe38c
mikalikahi <sweeney.phil@gmail.com>
2022-11-01 11:38:16 +0800

    Beginning of linked lists practice in abstract data types labs

---
+++ src/labs_examples/datastructures/linkedlist/labs/LinkedListImplementation.java
@@ -0,0 +1,19 @@
+package labs_examples.datastructures.linkedlist.labs;
+
+import java.util.LinkedList;
+
+public class LinkedListImplementation {
+    public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList();
+
+        linkedList.add("item0");
+        linkedList.add("item1");
+        linkedList.add("item2");
+        linkedList.add("item3");
+        linkedList.add("item4");
+        linkedList.add("item5");
+        linkedList.add("item6");
+        linkedList.add("item7");
+
+    }
+}
